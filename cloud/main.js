78cee5130511656f26978ed17044addc638d84d5
Takao Naito <naito@claudetech.com>
2015-03-26 16:32:20 +0900

    Put out mailer program.

---
+++ cloud/main.js
@@ -0,0 +1,25 @@
+Parse.Cloud.define("sendEmail", function(request, response) {
+  var sendgrid = require("sendgrid");
+  sendgrid.initialize("<%= sendgrid.username =>", "<%= sendgrid.password =>");
+
+  var name = request.params.name;
+  var email = request.params.email;
+  var message = request.params.message;
+
+  sendgrid.sendEmail({
+   to: "naito@claudetech.com",
+   from: "no-reply@claudetech.com",
+   fromname: "Claude Tech mail form",
+   subject: "Email from Claude Tech mail form",
+   text: "Name: "+name+"\nEmail: "+email+"\n---Message---\n"+message
+   }, {
+     success: function(httpResponse) {
+       console.log(httpResponse);
+       response.success("Email sent!");
+    },
+     error: function(httpResponse) {
+       console.error(httpResponse);
+       response.error("Uh oh, something went wrong");
+    }
+  });
+});
