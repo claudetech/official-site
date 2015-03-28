daae85b6cbb13f75b8092812c36c5258e1dbe4f4
Takao Naito <naito@claudetech.com>
2015-03-28 23:50:45 +0900

    Make mobile navigation, Put out head tags, Fix layout bugs.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,19 @@
-$(document).ready(function(){
+$(function(){
+
   skrollr.init({forceHeight: false});
+
+  $('.hamburger-icon a').click(function(){
+    $('._menu').toggle();
+  });
+
 });
 
 $(document).on('scroll', function(){
+
   if($(document).scrollTop() > 15){
     $('header').addClass('scrolled');
   } else {
     $('header').removeClass('scrolled');
   }
+
 });
