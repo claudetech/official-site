dd17e440d56e788f780c0ae676fc85e878ed9203
Yuya Soneoka <ej.a9p96@gmail.com>
2018-04-27 18:38:31 +0900

    Preparing to publish.

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(function(){
+
+  // skrollr.init({forceHeight: false});
+
+  $('.hamburger-icon a').click(function(){
+    $('._menu').toggle();
+  });
+
+});
+
+$(document).on('scroll', function(){
+
+  if($(document).scrollTop() > 15){
+    $('header').addClass('scrolled');
+  } else {
+    $('header').removeClass('scrolled');
+  }
+
+});
